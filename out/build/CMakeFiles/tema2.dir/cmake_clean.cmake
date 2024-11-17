37f6cbadce62dab5ed5287253cc9d9104214d370
ShadyRaccoon <marianbogdan953@yahoo.com>
2024-11-17 13:51:54 +0200

    modificari & adaugare build

---
+++ out/build/CMakeFiles/tema2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tema2.dir/item4.cpp.o"
+  "CMakeFiles/tema2.dir/item4.cpp.o.d"
+  "CMakeFiles/tema2.dir/item4_1.cpp.o"
+  "CMakeFiles/tema2.dir/item4_1.cpp.o.d"
+  "CMakeFiles/tema2.dir/item5.cpp.o"
+  "CMakeFiles/tema2.dir/item5.cpp.o.d"
+  "CMakeFiles/tema2.dir/item6.cpp.o"
+  "CMakeFiles/tema2.dir/item6.cpp.o.d"
+  "CMakeFiles/tema2.dir/main.cpp.o"
+  "CMakeFiles/tema2.dir/main.cpp.o.d"
+  "tema2"
+  "tema2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tema2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ out/build/CMakeFiles/tema2.dir/item6.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/tema2.dir/item6.cpp.o: \
+ /home/bogdan/Desktop/tema2paoo/item6.cpp /usr/include/stdc-predef.h
