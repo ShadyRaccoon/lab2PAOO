31ee83c3f85e633fd749839922d8904a4de57483
ShadyRaccoon <marianbogdan953@yahoo.com>
2024-11-17 09:43:35 +0200

    demo item4

--- main.cpp
+++ main.cpp
@@ -13,5 +13,27 @@ int main(){
 }
 
 void demoItem4(){
-    
+    //demonstratie constructori implicit cu lista de init/parametrizat
+    Business4 obj1;
+    Business4 obj2(20, 100);
+
+    std::cout << "ANGAJATI BUSINESS1 : " << obj1.getAngajati()
+              << "| COMENZI BUSINESS1 :" << obj1.getComenzi() << std::endl;
+
+    std::cout << "ANGAJATI BUSINESS2 : " << obj2.getAngajati()
+              << "| COMENZI BUSINESS2 :" << obj2.getComenzi() << std::endl;
+
+    //testare setters
+    obj1.setAngajati(25);
+    obj1.setComenzi(200);
+    std::cout << "ANGAJATI BUSINESS2 UPDATE: " << obj2.getAngajati()
+              << "| COMENZI BUSINESS2 UPDATE:" << obj2.getComenzi() << std::endl;
+
+    obj2.plaseazaComanda();
+    std::cout << "ANGAJATI BUSINESS2 UPDATE: " << obj2.getAngajati()
+              << "| COMENZI BUSINESS2 UPDATE:" << obj2.getComenzi() << std::endl;
+    std::cout << "VANZARI: " << Business4::vanzari(-1) << std::endl;
+
+    //prin folosirea metodelor care intorc valori statice se evita static init order fiasco
+    std::cout << "TAXE DE PLATIT: " << IRS4::taxes();
 }
